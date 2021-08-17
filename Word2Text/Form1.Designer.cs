31b86e5c1813185cb80fc74a916f0c5468d1b447
哲維 劉 <koo6541@hotmail.com>
2021-08-17 17:49:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Word2Text/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Word2Text
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_trans = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(599, 114);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(30, 29);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "…";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(214, 116);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(379, 27);
+            this.txt_path.TabIndex = 1;
+            // 
+            // btn_trans
+            // 
+            this.btn_trans.Location = new System.Drawing.Point(535, 149);
+            this.btn_trans.Name = "btn_trans";
+            this.btn_trans.Size = new System.Drawing.Size(94, 29);
+            this.btn_trans.TabIndex = 2;
+            this.btn_trans.Text = "入替";
+            this.btn_trans.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_trans);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button btn_trans;
+    }
+}
+
---
+++ Word2Text/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word2Text
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter = "WORDファイル(*.docx;*.doc)|*.docx;*.doc";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                txt_path.Text = ofd.FileName;
+            }
+        }
+    }
+}
