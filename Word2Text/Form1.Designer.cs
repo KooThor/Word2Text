feb4f0ef72a653ceff386794717837ad7f135c40
KooThor <koo6541@gmail.com>
2021-08-20 16:30:55 +0800

    テキストファイルの読み込みと書き込み

--- Word2Text/Form1.Designer.cs
+++ Word2Text/Form1.Designer.cs
@@ -32,7 +32,11 @@ namespace Word2Text
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btn_open = new System.Windows.Forms.Button();
             this.txt_path = new System.Windows.Forms.TextBox();
-            this.btn_trans = new System.Windows.Forms.Button();
+            this.btn_trans_txt = new System.Windows.Forms.Button();
+            this.rdo_txt = new System.Windows.Forms.RadioButton();
+            this.rdo_word = new System.Windows.Forms.RadioButton();
+            this.btn_trans_word = new System.Windows.Forms.Button();
+            this.lb_finish = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -41,7 +45,7 @@ namespace Word2Text
             // 
             // btn_open
             // 
-            this.btn_open.Location = new System.Drawing.Point(599, 114);
+            this.btn_open.Location = new System.Drawing.Point(397, 40);
             this.btn_open.Name = "btn_open";
             this.btn_open.Size = new System.Drawing.Size(30, 29);
             this.btn_open.TabIndex = 0;
@@ -51,26 +55,78 @@ namespace Word2Text
             // 
             // txt_path
             // 
-            this.txt_path.Location = new System.Drawing.Point(214, 116);
+            this.txt_path.Location = new System.Drawing.Point(12, 42);
             this.txt_path.Name = "txt_path";
             this.txt_path.Size = new System.Drawing.Size(379, 27);
             this.txt_path.TabIndex = 1;
             // 
-            // btn_trans
+            // btn_trans_txt
             // 
-            this.btn_trans.Location = new System.Drawing.Point(535, 149);
-            this.btn_trans.Name = "btn_trans";
-            this.btn_trans.Size = new System.Drawing.Size(94, 29);
-            this.btn_trans.TabIndex = 2;
-            this.btn_trans.Text = "入替";
-            this.btn_trans.UseVisualStyleBackColor = true;
+            this.btn_trans_txt.Location = new System.Drawing.Point(433, 40);
+            this.btn_trans_txt.Name = "btn_trans_txt";
+            this.btn_trans_txt.Size = new System.Drawing.Size(94, 29);
+            this.btn_trans_txt.TabIndex = 2;
+            this.btn_trans_txt.Text = "入替";
+            this.btn_trans_txt.UseVisualStyleBackColor = true;
+            this.btn_trans_txt.Click += new System.EventHandler(this.btn_trans_txt_Click);
+            // 
+            // rdo_txt
+            // 
+            this.rdo_txt.AutoSize = true;
+            this.rdo_txt.Checked = true;
+            this.rdo_txt.Location = new System.Drawing.Point(12, 12);
+            this.rdo_txt.Name = "rdo_txt";
+            this.rdo_txt.Size = new System.Drawing.Size(56, 24);
+            this.rdo_txt.TabIndex = 3;
+            this.rdo_txt.TabStop = true;
+            this.rdo_txt.Text = "Text";
+            this.rdo_txt.UseVisualStyleBackColor = true;
+            this.rdo_txt.CheckedChanged += new System.EventHandler(this.rdo_CheckedChanged);
+            // 
+            // rdo_word
+            // 
+            this.rdo_word.AutoSize = true;
+            this.rdo_word.Location = new System.Drawing.Point(74, 12);
+            this.rdo_word.Name = "rdo_word";
+            this.rdo_word.Size = new System.Drawing.Size(66, 24);
+            this.rdo_word.TabIndex = 4;
+            this.rdo_word.Text = "Word";
+            this.rdo_word.UseVisualStyleBackColor = true;
+            this.rdo_word.CheckedChanged += new System.EventHandler(this.rdo_CheckedChanged);
+            // 
+            // btn_trans_word
+            // 
+            this.btn_trans_word.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_trans_word.Location = new System.Drawing.Point(433, 40);
+            this.btn_trans_word.Name = "btn_trans_word";
+            this.btn_trans_word.Size = new System.Drawing.Size(94, 29);
+            this.btn_trans_word.TabIndex = 5;
+            this.btn_trans_word.Text = "入替";
+            this.btn_trans_word.UseVisualStyleBackColor = true;
+            this.btn_trans_word.Visible = false;
+            this.btn_trans_word.Click += new System.EventHandler(this.btn_trans_word_Click);
+            // 
+            // lb_finish
+            // 
+            this.lb_finish.AutoSize = true;
+            this.lb_finish.Font = new System.Drawing.Font("Yu Gothic UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_finish.Location = new System.Drawing.Point(12, 75);
+            this.lb_finish.Name = "lb_finish";
+            this.lb_finish.Size = new System.Drawing.Size(226, 35);
+            this.lb_finish.TabIndex = 6;
+            this.lb_finish.Text = "ファイル保存しました。";
+            this.lb_finish.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_trans);
+            this.ClientSize = new System.Drawing.Size(538, 126);
+            this.Controls.Add(this.lb_finish);
+            this.Controls.Add(this.btn_trans_word);
+            this.Controls.Add(this.rdo_word);
+            this.Controls.Add(this.rdo_txt);
+            this.Controls.Add(this.btn_trans_txt);
             this.Controls.Add(this.txt_path);
             this.Controls.Add(this.btn_open);
             this.Name = "Form1";
@@ -85,7 +141,11 @@ namespace Word2Text
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btn_open;
         private System.Windows.Forms.TextBox txt_path;
-        private System.Windows.Forms.Button btn_trans;
+        private System.Windows.Forms.Button btn_trans_txt;
+        private System.Windows.Forms.RadioButton rdo_txt;
+        private System.Windows.Forms.RadioButton rdo_word;
+        private System.Windows.Forms.Button btn_trans_word;
+        private System.Windows.Forms.Label lb_finish;
     }
 }
 
--- Word2Text/Form1.cs
+++ Word2Text/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,14 @@ namespace Word2Text
 
         private void btn_open_Click(object sender, EventArgs e)
         {
+            lb_finish.Visible = false;
+            string filter = rdo_txt.Checked ? "*.txt" : "*.doc;*.docx"; 
             //OpenFileDialogクラスのインスタンスを作成
             OpenFileDialog ofd = new OpenFileDialog();
 
             //[ファイルの種類]に表示される選択肢を指定する
             //指定しないとすべてのファイルが表示される
-            ofd.Filter = "WORDファイル(*.docx;*.doc)|*.docx;*.doc";
+            ofd.Filter = filter + "|" + filter;
             //[ファイルの種類]ではじめに選択されるものを指定する
             ofd.FilterIndex = 1;
             //タイトルを設定する
@@ -43,6 +46,57 @@ namespace Word2Text
             {
                 //OKボタンがクリックされたとき、選択されたファイル名を表示する
                 txt_path.Text = ofd.FileName;
+            }
+        }
+
+        private void btn_trans_txt_Click(object sender, EventArgs e)
+        {
+            string path = txt_path.Text;
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("ファイル存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Encoding sjisEnc = Encoding.GetEncoding("UTF-8");
+            StreamReader reader = new StreamReader(path, sjisEnc);
+            string r_txt = reader.ReadToEnd();
+            string w_txt = r_txt.Trim().Replace("\r\n", "%K%P");
+            reader.Close();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "テキストファイル|*.txt";
+            saveFileDialog1.Title = "Save an Text File";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Save File to .txt  
+                StreamWriter m_WriterParameter = new StreamWriter(fs);
+                m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+                m_WriterParameter.Write(w_txt);
+                m_WriterParameter.Flush();
+                m_WriterParameter.Close();
+                fs.Close();
+                lb_finish.Visible = true;
+            }
+        }
+
+        private void btn_trans_word_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdo_txt.Checked)
+            {
+                btn_trans_txt.Visible = true;
+                btn_trans_word.Visible = false;
+            }
+            else
+            {
+                btn_trans_txt.Visible = false;
+                btn_trans_word.Visible = true;
             }
         }
     }
